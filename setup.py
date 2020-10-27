bd77dde03281c4f37f356ea3d784469f45973c1e
Tyler Yep <tyep@cs.stanford.edu>
2020-10-27 15:07:37 -0700

    convert setup.py to metadata

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
-import setuptools
+""" setup.py """
+from setuptools import setup
 
-with open("README.md") as fh:
-    LONG_DESCRIPTION = fh.read()
-
-setuptools.setup(
-    name="stanfordkarel",
-    version="0.2.2",
-    author="Tyler Yep",
-    author_email="tyep10@gmail.com",
-    description="Official Stanford Karel library used in CS 106A",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/tyleryep/stanford-karel",
-    packages=["stanfordkarel"],
-    include_package_data=True,
-    python_requires=">=3.5",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
