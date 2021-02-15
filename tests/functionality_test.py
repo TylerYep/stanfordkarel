ed74bf6a5cbf9962efeb285ab5c759adf021385a
Tyler Yep <tyep@cs.stanford.edu>
2021-02-14 16:26:03 -0800

    karel ascii testing

--- tests/functionality_test.py
+++ tests/functionality_test.py
@@ -8,7 +8,7 @@ from conftest import PROBLEMS, STUDENT_CODE_DIR, TIMEOUT, execute_karel_code
 
 @pytest.mark.timeout(TIMEOUT)
 @pytest.mark.parametrize("problem_name", PROBLEMS)
-def test_karel_functionality(problem_name):
+def test_student_functionality(problem_name):
     code_file = os.path.join(STUDENT_CODE_DIR, problem_name + ".py")
     if os.path.exists(code_file):
         execute_karel_code(code_file)
